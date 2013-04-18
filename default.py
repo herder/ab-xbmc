b8e1d852cc4dfbcf2957a2d2a98fd5e97851975d
Niklas Herder <niklas.herder@aftonbladet.se>
2013-04-18 11:29:17 +0200

    Add dir functions which work slightly better

--- default.py
+++ default.py
@@ -1,118 +1,114 @@
-import urllib,urllib2,re,xbmcplugin,xbmcgui
+import urllib, urllib2, re, xbmcplugin, xbmcgui
 
 SERVICE_URL = "http://tv.aftonbladet.se"
 PARAMS = "service=json"
 
-MODE_CATEGORIES=1
-MODE_LIVE=2
-MODE_POPULAR=3
-MODE_PROGRAMS=4
-MODE_VIDEOLINKS=5
+MODE_CATEGORIES = 1
+MODE_LIVE = 2
+MODE_POPULAR = 3
+MODE_PROGRAMS = 4
+MODE_VIDEOLINKS = 5
+
 
 def STARTMENU():
-    addDir('Kategorier','',MODE_CATEGORIES,'')
-    addDir('LIVE','',MODE_LIVE,'')
-    addDir('Mest sett just nu','',MODE_POPULAR,'')
+    addDir('Kategorier', '', MODE_CATEGORIES, '')
+    addDir('LIVE', '', MODE_LIVE, '')
+    addDir('Mest sett just nu', '', MODE_POPULAR, '')
 
-def CATEGORIES(url):
-    print("Cat url: " + url)
-    addDir('Brott & Straff','',MODE_PROGRAMS,'')
-    addDir('Blah','',MODE_PROGRAMS,'')
-    addDir('Mest allt','',MODE_PROGRAMS,'')
+
+def CATEGORIES():
+    addDir('Brott & Straff', '', MODE_PROGRAMS, '')
+    addDir('Blah', '', MODE_PROGRAMS, '')
+    addDir('Mest allt', '', MODE_PROGRAMS, '')
+
 
 def PROGRAMS(url, name):
     pass
 
-def VIDEOLINKS(url,name):
-    req = urllib2.Request(url)
-    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-    response = urllib2.urlopen(req)
-    link=response.read()
-    response.close()
-    match=re.compile('').findall(link)
-    for url in match:
-        addLink(name,url,'')
 
+def VIDEOLINKS(url, name):
+    req = urllib2.Request(url)
+    req.add_header('User-Agent',
+                   'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+    response = urllib2.urlopen(req)
+    link = response.read()
+    response.close()
+    match = re.compile('').findall(link)
+    for url in match:
+        addLink(name, url, '')
 
 
 def get_params():
-    param=[]
-    paramstring=sys.argv[2]
-    if len(paramstring)>=2:
-        params=sys.argv[2]
-        cleanedparams=params.replace('?','')
-        if (params[len(params)-1]=='/'):
-            params=params[0:len(params)-2]
-        pairsofparams=cleanedparams.split('&')
-        param={}
+    param = []
+    paramstring = sys.argv[2]
+    if len(paramstring) >= 2:
+        params = sys.argv[2]
+        cleanedparams = params.replace('?', '')
+        if (params[len(params) - 1] == '/'):
+            params = params[0:len(params) - 2]
+        pairsofparams = cleanedparams.split('&')
+        param = {}
         for i in range(len(pairsofparams)):
-            splitparams={}
-            splitparams=pairsofparams[i].split('=')
-            if (len(splitparams))==2:
-                param[splitparams[0]]=splitparams[1]
+            splitparams = {}
+            splitparams = pairsofparams[i].split('=')
+            if (len(splitparams)) == 2:
+                param[splitparams[0]] = splitparams[1]
 
     return param
 
 
-
-
-def addLink(name,url,iconimage):
-    ok=True
-    liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
-    liz.setInfo( type="Video", infoLabels={ "Title": name } )
-    ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
+def addLink(name, url, iconimage):
+    ok = True
+    liz = xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
+    liz.setInfo(type="Video", infoLabels={"Title": name})
+    ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=liz)
     return ok
 
 
-def addDir(name,url,mode,iconimage):
-    u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
-    ok=True
-    liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
-    liz.setInfo( type="Video", infoLabels={ "Title": name } )
-    ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+def addDir(name, url, mode, iconimage):
+    u = sys.argv[0] + "?url=" + urllib.quote_plus(url) + "&mode=" + str(mode) + "&name=" + urllib.quote_plus(name)
+    ok = True
+    liz = xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+    liz.setInfo(type="Video", infoLabels={"Title": name})
+    ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=liz, isFolder=True)
     return ok
 
 
-params=get_params()
-url=None
-name=None
-mode=None
+params = get_params()
+url = None
+name = None
+mode = None
 
 try:
-    url=urllib.unquote_plus(params["url"])
+    url = urllib.unquote_plus(params["url"])
 except:
     pass
 try:
-    name=urllib.unquote_plus(params["name"])
+    name = urllib.unquote_plus(params["name"])
 except:
     pass
 try:
-    mode=int(params["mode"])
+    mode = int(params["mode"])
 except:
     pass
 
-print "Mode: "+str(mode)
-print "URL: "+str(url)
-print "Name: "+str(name)
+print "Mode: " + str(mode)
+print "URL: " + str(url)
+print "Name: " + str(name)
 
-
-
-if mode==None or url==None or len(url)<1:
+if mode == None:
     print ""
     STARTMENU()
 
-elif mode==MODE_CATEGORIES:
-    print ""+url
-    CATEGORIES(url)
+elif mode == MODE_CATEGORIES:
+    CATEGORIES()
 
-elif mode==MODE_PROGRAMS:
-    print ""+url
-    PROGRAMS(url,name)
+elif mode == MODE_PROGRAMS:
+    print "" + url
+    PROGRAMS(url, name)
 
-elif mode==MODE_VIDEOLINKS:
-    print ""+url
-    VIDEOLINKS(url,name)
-
-
+elif mode == MODE_VIDEOLINKS:
+    print "" + url
+    VIDEOLINKS(url, name)
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
